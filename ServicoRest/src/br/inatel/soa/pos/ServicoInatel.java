9cb0eebfeb3997a5d7b5f03a1a3d3c152cb2ca2f
Danilo Carvalho <danilo.carvalho@inatel.br>
2021-04-24 15:01:50 -0300

    Add POST operation

--- ServicoRest/src/br/inatel/soa/pos/ServicoInatel.java
+++ ServicoRest/src/br/inatel/soa/pos/ServicoInatel.java
@@ -11,6 +11,7 @@
 package br.inatel.soa.pos;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -26,5 +27,13 @@ public class ServicoInatel
     {
         return "{\"Hello World\":\"" + name + "\"}";
     }
-    
+
+    @POST
+    @Produces ("application/json")
+    @Path ("teste/{nome}")
+    public String alo2World(@PathParam ("nome") String name)
+    {
+        return "{\"Hello World 2\":\"" + name + "\"}";
+    }
+
 }
