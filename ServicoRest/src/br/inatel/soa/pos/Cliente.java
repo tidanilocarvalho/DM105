6dab75ae478be83ef55f42acdfd7f7085f01c250
Danilo Carvalho <danilo.carvalho@inatel.br>
2021-04-24 15:21:51 -0300

    Add client

---
+++ ServicoRest/src/br/inatel/soa/pos/Cliente.java
@@ -0,0 +1,52 @@
+package br.inatel.soa.pos;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Cliente
+{
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public static void main(String[] args) throws Exception
+    {
+        Cliente http = new Cliente();
+        http.sendGet();
+    }
+
+    private void sendGet() throws Exception
+    {
+        String url = "http://localhost:8080/ServicoRest/api/alo/teste/oi";
+       
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+       
+        // optional default is GET
+       
+        con.setRequestMethod("GET");
+        //add request header
+        
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = con.getResponseCode();
+        
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+       
+        String inputLine;
+        
+        StringBuffer response = new StringBuffer();
+        
+        while ((inputLine = in.readLine()) != null)
+        {
+            response.append(inputLine);
+        }
+        
+        in.close();
+        
+        //print result
+        System.out.println(response.toString());
+    }
+}
