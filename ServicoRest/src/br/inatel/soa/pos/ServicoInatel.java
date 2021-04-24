467fba5bd21d13a73d2a6a99628cb39d4037647f
Danilo Carvalho <danilo.carvalho@inatel.br>
2021-04-24 14:51:18 -0300

    Add first WADL project

---
+++ ServicoRest/src/br/inatel/soa/pos/ServicoInatel.java
@@ -0,0 +1,28 @@
+//**********************************************************************
+// Copyright (c) 2021 Telefonaktiebolaget LM Ericsson, Sweden.
+// All rights reserved.
+// The Copyright to the computer program(s) herein is the property of
+// Telefonaktiebolaget LM Ericsson, Sweden.
+// The program(s) may be used and/or copied with the written permission
+// from Telefonaktiebolaget LM Ericsson or in accordance with the terms
+// and conditions stipulated in the agreement/contract under which the
+// program(s) have been supplied.
+// **********************************************************************
+package br.inatel.soa.pos;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path ("alo")
+public class ServicoInatel
+{
+
+    @Produces ("application/json")
+    @Path ("teste/{nome}")
+    public String aloWorld(@PathParam ("nome") String name)
+    {
+        return "{\"Hello World\":\"" + name + "\"}";
+    }
+    
+}
